eb9c62613bf110d0ae42b9166a8f4bd11a744a6d
PayalMajumder <payalmzr@gmail.com>
2024-07-30 17:14:18 +0100

    Files added

---
+++ src/ex_30072024/Lab001.java
@@ -0,0 +1,8 @@
+package ex_30072024;
+
+public class Lab001 {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
+// write instructions here
---
+++ src/ex_30072024/Lab002.java
@@ -0,0 +1,14 @@
+package ex_30072024;
+
+public class Lab002 {
+    public static void main (String[]args) {
+        System.out.println("Hello Class!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(3/2);
+        System.out.println(2*3);
+
+
+
+
+}
