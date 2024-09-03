1df9b151f440e1e26195ddec061db1b38b2e3630
PayalMajumder <payalmzr@gmail.com>
2024-09-03 23:48:31 +0530

    Files added 72

---
+++ src/ex_03092024_Loops/Lab072.java
@@ -0,0 +1,14 @@
+package ex_03092024_Loops;
+
+public class Lab072 {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            if (i%2==0)l
+            {
+                System.out.println("Even-" + i);
+                continue;
+            }
+            System.out.println("Odd-" + i);
+        }
+    }
+}
